9da4b5c8d2b9fab1cdee236e652d536f954ac46b
roscas <sol.rosca@he-arc.ch>
2018-08-01 23:30:50 +0200

    public: fix addReviewButton

--- src/components/Public/Review/Review.vue
+++ src/components/Public/Review/Review.vue
@@ -1,26 +1,39 @@
 <template>
-  <section id="Galerie" class="section section-container">
-    <div class="container">
-      <div class="content">
-        <Title>{{ title }}</Title>
-        <p class="sub-title">{{ subTi }}</p>
-        <div class="level">
+  <div>
+    <section id="Galerie" class="section section-container">
+      <EditIcon top="20px" :component="name">
+        <div class="container">
+          <div class="content">
+            <Title :class="{'highlighted': highlighted(1)}">
+              {{ title }}
+            </Title>
+            <p class="sub-title"
+               :class="{'highlighted': highlighted(2)}">
+              {{ subTi }}
+            </p>
+          </div>
+        </div>
+      </EditIcon>
+      <div class="container">
+        <div class="content">
 
-          <div class="level-item">
-            <div class="level is-mobile overall">
-              <div class="level-left"
-                   v-scroll-reveal="{
+          <div class="level">
+
+            <div class="level-item">
+              <div class="level is-mobile overall">
+                <div class="level-left"
+                     v-scroll-reveal="{
                     origin: 'left',
                     distance: '100px',
                     duration: 1500,
                     delay: 0,
                     easing: 'ease',
                    }">
-                {{ overall }}
-              </div>
-              <div class="level-right">
-                <div class="level-item"
-                     v-scroll-reveal="{
+                  {{ overall }}
+                </div>
+                <div class="level-right">
+                  <div class="level-item"
+                       v-scroll-reveal="{
                       origin: 'top',
                       distance: '0px',
                       duration: 1500,
@@ -31,80 +44,98 @@
                     <i class="fas fa-star" v-if="star == 1"></i>
                     <i class="fas fa-star-half" v-if="star == 2"></i>
                   </span>
-                </div>
-                <div class="leve-item"
-                     v-scroll-reveal="{
+                  </div>
+                  <div class="leve-item"
+                       v-scroll-reveal="{
                       origin: 'right',
                       distance: '100px',
                       duration: 1500,
                       delay: 0,
                       easing: 'ease',
                      }">
-                  <a target="_blank" :href="reviewAll">80+ reviews</a>
+                    <a target="_blank" :href="reviewAll">80+ reviews</a>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
-          <div class="level-right"
-               v-scroll-reveal="{
+              <div class="level-right"
+                   v-scroll-reveal="{
                 origin: 'left',
                 distance: '100px',
                 duration: 3500,
                 delay: 500,
                 easing: 'ease',
                }">
-            <a target="_blank" :href="reviewNew" class="button _btn no-tr">
-              <i class="fas fa-pencil-alt"></i>
-              &nbsp; Ajouter une review
-            </a>
+                <a target="_blank" :href="reviewNew"
+                   class="button _btn no-tr">
+                  <i class="fas fa-pencil-alt"></i>
+                  &nbsp; Ajouter une review
+                </a>
+              </div>
+            </div>
           </div>
-        </div>
 
 
-        <div class="columns is-multiline">
-          <div class="column is-6"
-               v-for="review in reviews"
-               v-scroll-reveal="{
+          <div class="columns is-multiline">
+            <div class="column is-6"
+                 v-for="review in reviews"
+                 v-scroll-reveal="{
                 origin: 'bottom',
                 distance: '100px',
                 duration: 1500,
                 delay: 0,
                 easing: 'ease',
              }">
-            <div class="card shadow">
-              <div class="card-content">
-                <div class="media">
-                  <div class="media-left">
-                    <figure class="image is-48x48">
-                      <img :src="review.profile_photo_url">
-                    </figure>
-                  </div>
-                  <div class="media-content">
+              <div class="card shadow">
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-left">
+                      <figure class="image is-48x48">
+                        <img :src="review.profile_photo_url">
+                      </figure>
+                    </div>
+                    <div class="media-content">
                   <span class="stars"
                         v-for="star in getStarsList(review.rating)">
                     <i class="full fas fa-star" v-if="star == 1"></i>
                     <i class="empty fas fa-star" v-if="star == 0"></i>
                   </span>
-                    <br>
-                    <a :href="review.author_url" class="subtitle is-6">{{
-                      review.author_name }}</a>
-                    &nbsp; {{ getDateTime(review.time) }}
+                      <br>
+                      <a :href="review.author_url" class="subtitle is-6">{{
+                        review.author_name }}</a>
+                      &nbsp; {{ getDateTime(review.time) }}
+                    </div>
                   </div>
-                </div>
 
-                <div class="content">
-                  {{ review.text }}
-                  {{ review.rating }}
-                  <br>
+                  <div class="content">
+                    {{ review.text }}
+                    {{ review.rating }}
+                    <br>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
 
+    <EditNav v-if="checkName()" height="225">
+      <b-tabs v-model="activeTab" position="is-centered">
+        <b-tab-item label="Titre">
+          <b-input maxlength="35"
+                   :disabled="loading"
+                   v-model="title">
+          </b-input>
+        </b-tab-item>
+        <b-tab-item label="Sous titre">
+          <b-input maxlength="200"
+                   :disabled="loading"
+                   v-model="subTi">
+          </b-input>
+        </b-tab-item>
+      </b-tabs>
+    </EditNav>
+  </div>
 </template>
 
 <script>
