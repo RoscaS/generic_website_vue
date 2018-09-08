2f3b766abfaf40c4a6711a02372d8e0117d7176b
roscas <sol.rosca@he-arc.ch>
2018-09-08 19:13:15 +0200

    maj dist

--- src/http.js
+++ src/http.js
@@ -1,7 +1,8 @@
 import Axios from "axios";
 
 const axios = Axios.create({});
-axios.defaults.baseURL = "http://localhost:8000/";
+// axios.defaults.baseURL = "http://localhost:8000/";
 // axios.defaults.baseURL = "http://jrosk.ch:8000/";
+axios.defaults.baseURL = "http://api.jrosk.ch/";
 
 export default axios;
