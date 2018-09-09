ef3d9552467e2cd6cd935b08233912b08d025e2f
roscas <sol.rosca@he-arc.ch>
2018-09-09 15:02:52 +0200

    opening hours refactor & add closed

--- src/http.js
+++ src/http.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 const axios = Axios.create({});
-axios.defaults.baseURL = "http://localhost:8000/";
-// axios.defaults.baseURL = "https://api.jrosk.ch/";
+// axios.defaults.baseURL = "http://localhost:8000/";
+axios.defaults.baseURL = "https://api.jrosk.ch/";
 
 export default axios;
