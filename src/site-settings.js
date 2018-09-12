009ab7049536c06765e617727941c16181ed9986
roscas <sol.rosca@he-arc.ch>
2018-09-12 02:33:37 +0200

    pre prod CJ

--- src/site-settings.js
+++ src/site-settings.js
@@ -1,8 +1,8 @@
-const admin = "admin@jura-coiffure.com";
-const url = "https://www.jura-coiffure-api.jrosk.ch/";
-const clientID = "jDtlSDX4Sr4CxX9C4jD4uHxe5wrNpNnZ";
-const redirectUri = "https://www.jura-coiffure.jrosk.ch/callback";
-const audience = "https://jura-coiffure-api.jrosk.ch";
+const admin = "admin@jrosk.com";
+const url = "https://www.api.jrosk.ch/";
+const clientID = "3zna8lwrR2rHoWxxwQHEqgRn6dPezrcI";
+const redirectUri = "https://www.jrosk.ch/callback";
+const audience = "http://jrosk.ch:8000";
 
 const settings = {
 	admin: admin,
@@ -13,3 +13,19 @@ const settings = {
 };
 
 export default settings
+
+
+
+// const admin = "admin@jrosk.com";
+// const url = "https://www.api.jrosk.ch/";
+// const clientID = "3zna8lwrR2rHoWxxwQHEqgRn6dPezrcI";
+// const redirectUri = "https://www.jrosk.ch/callback";
+// const audience = "http://jrosk.ch:8000";
+
+
+
+// const admin = "admin@jura-coiffure.com";
+// const url = "https://www.jura-coiffure-api.jrosk.ch/";
+// const clientID = "jDtlSDX4Sr4CxX9C4jD4uHxe5wrNpNnZ";
+// const redirectUri = "https://www.jura-coiffure.jrosk.ch/callback";
+// const audience = "https://jura-coiffure-api.jrosk.ch";
