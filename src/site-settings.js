847bf997c3b870db5d9adc558f983d0117293484
roscas <sol.rosca@he-arc.ch>
2018-09-12 02:24:35 +0200

    "maj dist"

--- src/site-settings.js
+++ src/site-settings.js
@@ -1,8 +1,8 @@
-const admin = "admin@jrosk.com";
-const url = "https://www.api.jrosk.ch/";
-const clientID = "3zna8lwrR2rHoWxxwQHEqgRn6dPezrcI";
-const redirectUri = "https://www.jrosk.ch/callback";
-const audience = "http://jrosk.ch:8000";
+const admin = "admin@jura-coiffure.com";
+const url = "https://www.jura-coiffure-api.jrosk.ch/";
+const clientID = "jDtlSDX4Sr4CxX9C4jD4uHxe5wrNpNnZ";
+const redirectUri = "https://www.jura-coiffure.jrosk.ch/callback";
+const audience = "https://jura-coiffure-api.jrosk.ch";
 
 const settings = {
 	admin: admin,
