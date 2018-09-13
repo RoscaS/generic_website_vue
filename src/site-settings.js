17eb9a51a2497caec9f28d4c57daaf9cab1b406d
roscas <sol.rosca@he-arc.ch>
2018-09-13 03:56:50 +0200

    "maj dist"

--- src/site-settings.js
+++ src/site-settings.js
@@ -1,16 +1,16 @@
-const admin = "admin@jrosk.com";
-const url = "https://www.api.jrosk.ch/";
-const clientID = "3zna8lwrR2rHoWxxwQHEqgRn6dPezrcI";
-const redirectUri = "https://www.jrosk.ch/callback";
-const audience = "http://jrosk.ch:8000";
-const logo = "";
-const lat = "47.22576979999999";
-const long = "6.957600500000012";
+const admin = "admin@jura-coiffure.com";
+const url = "https://www.jura-coiffure-api.jrosk.ch/";
+const clientID = "jDtlSDX4Sr4CxX9C4jD4uHxe5wrNpNnZ";
+const redirectUri = "https://www.jura-coiffure.jrosk.ch/callback";
+const audience = "https://jura-coiffure-api.jrosk.ch";
+const logo = "jura_coiffure.png";
+const lat = "47.22439319999999";
+const long = "6.960135600000058";
 const mapBox = "pk.eyJ1Ijoicm9zY2FzIiwiYSI6ImNqbGlka2p2YjAwNGEzcG5qdGltMDR0aDgifQ.Z0Fl0VypHnN0gLMU50W2BQ";
-const title = "Generic Website For All";
-const description = "Dev Staging";
-const keywords = "";
-const canonical = "https://www.jrosk.ch";
+const title = "Jura coiffure";
+const description = "Salon de coiffure situé au Noirmont dans le Jura Suisse. Coupe homme et femme avec et sans randez-vous.";
+const keywords = "coiffure, barbier, homme, femme, sans rendez-vous";
+const canonical = "https://www.jura-coiffure.jrosk.ch";
 
 
 
