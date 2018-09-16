497c570293a4e462a59784ee79ef4f017387202f
roscas <sol.rosca@he-arc.ch>
2018-09-16 21:52:09 +0200

    pre jura_coiffure prod

--- src/site-settings.js
+++ src/site-settings.js
@@ -1,16 +1,16 @@
 const admin = "jura.coiffure@gmail.com";
-const url = "http://api.jura-coiffure.ch/";
+const url = "https://api.jura-coiffure.ch/";
 const clientID = "6zWK4ZtfpGhXjUqaFENbVv0US2r5BZYq";
-const redirectUri = "http://jura-coiffure.ch/callback";
+const redirectUri = "https://jura-coiffure.ch/callback";
 const audience = "https://jura-coiffure.ch";
 const logo = "jura_coiffure.png";
 const lat = "47.22439319999999";
 const long = "6.960135600000058";
-const mapBox = "pk.eyJ1Ijoicm9zY2FzIiwiYSI6ImNqbGlka2p2YjAwNGEzcG5qdGltMDR0aDgifQ.Z0Fl0VypHnN0gLMU50W2BQ";
 const title = "Jura coiffure";
 const description = "Salon de coiffure situé au Noirmont dans le Jura Suisse. Coupe homme et femme avec et sans randez-vous.";
 const keywords = "coiffure, barbier, homme, femme, sans rendez-vous";
 const canonical = "https://jura-coiffure.ch";
+const mapBox = "pk.eyJ1IjoianJvc2siLCJhIjoiY2ptNTZldHZyMGticzN2bzQ1M2M1Z3I4MyJ9.9ExtxPYSahrO4IrvBwxrAA";
 
 const settings = {
   admin: admin,
