42f859f7cc3bf0bff0d7829ed89eccb1748f1e59
roscas <sol.rosca@he-arc.ch>
2018-09-17 15:09:15 +0200

    jura-coiffure deploy

--- src/auth.js
+++ src/auth.js
@@ -1,124 +1,126 @@
-import Tools from "./utils/tools";
-import settings from "./site-settings";
+import Tools from './utils/tools'
+import settings from './site-settings'
 
-import auth0 from "auth0-js";
-import Vue from "vue";
-import axios from "axios";
+import auth0 from 'auth0-js'
+import Vue from 'vue'
+import axios from 'axios'
 
 let webAuth = new auth0.WebAuth({
-  domain: "jrosk.eu.auth0.com",
+  domain: 'jrosk.eu.auth0.com',
   clientID: settings.clientID,
   redirectUri: settings.redirectUri,
   audience: settings.audience,
-  responseType: "token id_token",
-  scope: "openid profile"
-});
-
+  responseType: 'token id_token',
+  scope: 'openid profile',
+})
 
 let auth = new Vue({
   data: () => ({
-    tokenIsSet: false
+    tokenIsSet: false,
   }),
   computed: {
     token: {
-      get() {
-        return localStorage.getItem("id_token");
+      get () {
+        return localStorage.getItem('id_token')
       },
-      set(id_token) {
-        localStorage.setItem("id_token", id_token);
-      }
+      set (id_token) {
+        localStorage.setItem('id_token', id_token)
+      },
     },
     accessToken: {
-      get() {
-        return localStorage.getItem("access_token");
+      get () {
+        return localStorage.getItem('access_token')
       },
-      set(accessToken) {
-        localStorage.setItem("access_token", accessToken);
-      }
+      set (accessToken) {
+        localStorage.setItem('access_token', accessToken)
+      },
     },
     expiresAt: {
-      get() {
-        return localStorage.getItem("expires_at");
+      get () {
+        return localStorage.getItem('expires_at')
       },
-      set(expiresIn) {
-        let expiresAt = JSON.stringify(expiresIn * 1000 + new Date().getTime());
-        localStorage.setItem("expires_at", expiresAt);
-      }
+      set (expiresIn) {
+        let expiresAt = JSON.stringify(expiresIn * 1000 + new Date().getTime())
+        localStorage.setItem('expires_at', expiresAt)
+      },
     },
     user: {
-      get() {
-        return JSON.parse(localStorage.getItem("user"));
+      get () {
+        return JSON.parse(localStorage.getItem('user'))
       },
-      set(user) {
-        localStorage.setItem("user", JSON.stringify(user));
-      }
+      set (user) {
+        localStorage.setItem('user', JSON.stringify(user))
+      },
     },
     scope: {
-      get() {
-        return JSON.parse(localStorage.getItem("scope"));
+      get () {
+        return JSON.parse(localStorage.getItem('scope'))
       },
-      set(scope) {
-        localStorage.setItem("scope", JSON.stringify(scope));
-      }
-    }
+      set (scope) {
+        localStorage.setItem('scope', JSON.stringify(scope))
+      },
+    },
   },
   methods: {
-    setToken() {
+    setToken () {
       if (this.accessToken && !this.tokenIsSet) {
-        let token = `Bearer ${auth.accessToken}`;
-        axios.defaults.headers.common["Authorization"] = token;
-        this.tokenIsSet = true;
+        let token = `Bearer ${auth.accessToken}`
+        axios.defaults.headers.common['Authorization'] = token
+        this.tokenIsSet = true
       }
     },
-    login() {
-      webAuth.authorize();
+    login () {
+      webAuth.authorize()
     },
-    logout() {
+    logout () {
       return new Promise((resolve, reject) => {
-        localStorage.removeItem("access_token");
-        localStorage.removeItem("id_token");
-        localStorage.removeItem("expires_at");
-        localStorage.removeItem("user");
-        this.tokenIsSet = false;
-        webAuth.logout();
-      });
+        localStorage.removeItem('access_token')
+        localStorage.removeItem('id_token')
+        localStorage.removeItem('expires_at')
+        localStorage.removeItem('user')
+        this.tokenIsSet = false
+        webAuth.logout()
+      })
     },
-    isAuthenticated() {
-      return new Date().getTime() < this.expiresAt;
+    isAuthenticated () {
+      return new Date().getTime() < this.expiresAt
     },
-    handleAuthentication() {
+    handleAuthentication () {
       return new Promise((resolve, reject) => {
         webAuth.parseHash((err, authResult) => {
           if (authResult && authResult.accessToken && authResult.idToken) {
-            console.log(name)
+            console.log(authResult.idTokenPayload.name !== settings.admin ||
+                          authResult.idTokenPayload.name !== settings.superAdmin)
+
+            console.log(authResult.idTokenPayload.name)
             console.log(settings.admin)
             console.log(settings.superAdmin)
-            let name = authResult.idTokenPayload.name;
-            if (name !== settings.admin || name !== settings.superAdmin) {
-              this.logout();
-              Tools.message("authUserNotAllowed");
+            if (authResult.idTokenPayload.name !== settings.admin ||
+              authResult.idTokenPayload.name !== settings.superAdmin) {
+              this.logout()
+              Tools.message('authUserNotAllowed')
             } else {
-              this.expiresAt = authResult.expiresIn;
-              this.accessToken = authResult.accessToken;
-              this.token = authResult.idToken;
-              this.user = authResult.idTokenPayload;
+              this.expiresAt = authResult.expiresIn
+              this.accessToken = authResult.accessToken
+              this.token = authResult.idToken
+              this.user = authResult.idTokenPayload
 
-              this.setToken();
-              resolve();
+              this.setToken()
+              resolve()
             }
           } else if (err) {
-            console.log("err");
-            this.logout();
-            reject(err);
+            console.log('err')
+            this.logout()
+            reject(err)
           }
-        });
-      });
-    }
-  }
-});
+        })
+      })
+    },
+  },
+})
 
 export default {
-  install: function(Vue) {
-    Vue.prototype.$auth = auth;
-  }
-};
+  install: function (Vue) {
+    Vue.prototype.$auth = auth
+  },
+}
