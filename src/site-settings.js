1d15372274b86a646574093c64d045c000bc71ce
roscas <sol.rosca@he-arc.ch>
2019-03-01 14:33:30 +0100

    update

--- src/site-settings.js
+++ src/site-settings.js
@@ -1,16 +1,16 @@
 const admin = "admin@jrosk.com";
-const url = "https://www.api.jrosk.ch/";
+const url = "http://localhost:8000/";
 const clientID = "3zna8lwrR2rHoWxxwQHEqgRn6dPezrcI";
-const redirectUri = "https://www.jrosk.ch/callback";
+const redirectUri = "http://localhost:8080/callback";
 const audience = "http://jrosk.ch:8000";
 const logo = "";
 const lat = "47.22576979999999";
 const long = "6.957600500000012";
 const mapBox = "pk.eyJ1Ijoicm9zY2FzIiwiYSI6ImNqbGlka2p2YjAwNGEzcG5qdGltMDR0aDgifQ.Z0Fl0VypHnN0gLMU50W2BQ";
 const title = "Generic Website For All";
-const description = "Dev Staging";
+const description = "Dev local";
 const keywords = "";
-const canonical = "https://www.jrosk.ch";
+const canonical = "http://localhost:8080/";
 
 const settings = {
   superAdmin: 'jrosk.managment@gmail.com',
